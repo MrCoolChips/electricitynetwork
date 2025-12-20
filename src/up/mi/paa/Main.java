d0c23e556d6f526aa96c9e3d7ef7e852504acfdc
Ali <shouni007@gmail.com>
2025-12-20 13:36:53 +0100

    Mise a jour de la classe Main - Si un argument n'est pas valide, montrer l'aide

--- src/up/mi/paa/Main.java
+++ src/up/mi/paa/Main.java
@@ -13,11 +13,21 @@ public class Main {
     public static void main(String[] args) {
         if (args.length == 0) { MenuCLI.lancer(args); return; }
 
-        switch (args[0].toLowerCase()) {
-            case "--help", "-h"       -> MenuCLI.afficherAide();
-            case "--version", "-v"    -> System.out.println("Gestionnaire de Réseau Électrique v1.0.0\nGroupe 10 - PAA S5 2025");
-            case "--gui", "-g"        -> Application.launch(ReseauElectriqueUI.class);
-            default                   -> MenuCLI.lancer(args);
+        String arg0 = args[0].toLowerCase();
+        
+        if (arg0.startsWith("-")) {
+            switch (arg0) {
+                case "--help", "-h"       -> MenuCLI.afficherAide();
+                case "--version", "-v"    -> System.out.println("Gestionnaire de Réseau Électrique v1.0.0\nGroupe 10 - PAA S5 2025");
+                case "--gui", "-g"        -> Application.launch(ReseauElectriqueUI.class);
+                case "--cli", "-c"        -> MenuCLI.lancer(java.util.Arrays.copyOfRange(args, 1, args.length));
+                default -> {
+                    System.err.println("Option invalide : " + args[0]);
+                    MenuCLI.afficherAide();
+                }
+            }
+        } else {
+            MenuCLI.lancer(args);
         }
     }
 }
