6e386deb3815062f9db33d24cf0e774450b44b2b
Ali <shouni007@gmail.com>
2025-12-21 06:57:30 +0100

    Correction de la version de l'app

--- src/up/mi/paa/Main.java
+++ src/up/mi/paa/Main.java
@@ -18,7 +18,7 @@ public class Main {
         if (arg0.startsWith("-")) {
             switch (arg0) {
                 case "--help", "-h"       -> MenuCLI.afficherAide();
-                case "--version", "-v"    -> System.out.println("Gestionnaire de Réseau Électrique v1.0.0\nGroupe 10 - PAA S5 2025");
+                case "--version", "-v"    -> System.out.println("Gestionnaire de Réseau Électrique v2.0.0\nGroupe 10 - PAA S5 2025");
                 case "--gui", "-g"        -> Application.launch(ReseauElectriqueUI.class);
                 case "--cli", "-c"        -> MenuCLI.lancer(java.util.Arrays.copyOfRange(args, 1, args.length));
                 default -> {
